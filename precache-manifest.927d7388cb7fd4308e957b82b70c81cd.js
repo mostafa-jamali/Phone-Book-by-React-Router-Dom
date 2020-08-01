1b7d28b1b2fc3e9a1f8034dac81ed177cb22c657
mostafa jamali <m.jamali20274@gmail.com>
2020-08-02 01:30:32 +0430

    Updates

---
+++ precache-manifest.927d7388cb7fd4308e957b82b70c81cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b731b24509971b8d8b19b3eea855a98",
+    "url": "/Phone-Book-by-React-Router-Dom/index.html"
+  },
+  {
+    "revision": "30a90cd904019aa5f742",
+    "url": "/Phone-Book-by-React-Router-Dom/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "98b4e1940e01a7fc1ba8",
+    "url": "/Phone-Book-by-React-Router-Dom/static/css/main.6be825cb.chunk.css"
+  },
+  {
+    "revision": "30a90cd904019aa5f742",
+    "url": "/Phone-Book-by-React-Router-Dom/static/js/2.722a8e02.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Phone-Book-by-React-Router-Dom/static/js/2.722a8e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b4e1940e01a7fc1ba8",
+    "url": "/Phone-Book-by-React-Router-Dom/static/js/main.ad159299.chunk.js"
+  },
+  {
+    "revision": "4f2c536e363ec3b0a8cc",
+    "url": "/Phone-Book-by-React-Router-Dom/static/js/runtime-main.57ec6c1f.js"
+  }
+]);
